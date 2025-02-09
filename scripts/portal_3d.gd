452c1b39c595c5a443711f257c3cd96d634d2f09
Antoine Luciani <gurlux@icloud.com>
2025-02-09 11:15:35 +0100

    fix: Too lazy to work on Portal3D

--- scripts/portal_3d.gd
+++ scripts/portal_3d.gd
@@ -1,16 +1,19 @@
-@tool
 class_name Portal3D extends Node3D
 
 @export var camera: Camera3D
-@export var viewport: SubViewport
+@export var viewport_texture: ViewportTexture
 @export var portal_mesh: MeshInstance3D
 @export var target_portal: Portal3D
 
-var camera_feed: CameraFeed
-
-
 func _ready() -> void:
-	var viewport_texture := ViewportTexture.new()
-	viewport_texture.viewport_path = viewport.get_path()
-	(portal_mesh.get_surface_override_material(0) as StandardMaterial3D).albedo_texture = viewport_texture
+	(portal_mesh.get_surface_override_material(0) as StandardMaterial3D).albedo_texture = target_portal.viewport_texture
+
+func _process(delta: float) -> void:
+	var current_camera := get_viewport().get_camera_3d()
 	
+	var relative_pos := global_position - current_camera.global_position
+	var relative_rot := global_rotation - current_camera.global_rotation
+	camera.position = target_portal.global_position + relative_pos
+	camera.global_rotation = target_portal.global_rotation
+
+	prints(relative_pos, relative_rot)
